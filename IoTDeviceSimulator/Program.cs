46c0c06a7864e6b4ddc4def509935a70c9aa8664
Tatiana <61848549+Fhordone@users.noreply.github.com>
2023-06-19 10:41:10 -0500

    Simulator1
    Este simulador envía data del ID y estado en el que se encuentre el dispositivo (1 o 0) para simular el dispositivo IoT que se usará para el presente proyecto.

---
+++ IoTDeviceSimulator/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IoTDeviceSimulator
+{
+    class Program
+    {
+        private static DeviceClient deviceClient;
+        private static string connectionString;
+        private static int deviceId = 1;
+        private static Random random = new Random();
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("IoT Device Simulator");
+
+            // Configuración
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            connectionString = config.GetConnectionString("IoTHubConnectionString");
+            deviceClient = DeviceClient.CreateFromConnectionString(connectionString, TransportType.Mqtt);
+
+            // Simulación
+            while (true)
+            {
+                int state = random.Next(0, 2);
+                await SendDeviceToCloudMessageAsync(deviceId, state);
+                Console.WriteLine($"Sent message - DeviceId: {deviceId}, State: {state}");
+
+                await Task.Delay(300000); // Espera 5 minutos antes de enviar el siguiente mensaje
+            }
+        }
+
+        private static async Task SendDeviceToCloudMessageAsync(int deviceId, int state)
+        {
+            var telemetryDataPoint = new
+            {
+                DeviceId = deviceId,
+                State = state
+            };
+
+            var messageString = Newtonsoft.Json.JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.UTF8.GetBytes(messageString));
+
+            await deviceClient.SendEventAsync(message);
+        }
+    }
+}
